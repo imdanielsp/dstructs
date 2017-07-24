8c0406d5a1a97e54e6930caad57a3aa4c3c976f4
Daniel Santos <dsantosp12@gmail.com>
2017-07-24 07:06:27 -0400

    Improved build process and created CMake files for test and src separate.

--- build.sh
+++ build.sh
@@ -2,34 +2,28 @@
 
 if [ $1 ]
 then
-    if [ $1 == "--build-tests" ]
+    if [ $1 == "run-tests" ]
     then
         echo "Building tests..."
+        cd ./test/
         cmake -B./build/ -H.
         echo "Compiling tests..."
         make -C ./build/
         ./build/dstructs-test
         result=$?
     fi
-    if [ $1 == "-build" ]
+    if [ $1 == "build" ]
     then
 	echo "Building main..."
+	cd ./src/
 	cmake -B./build/ -H.
 	echo "Compiling..."
 	make -C ./build/
 	result=$?
     fi
-else
-    echo "No argument provided"
-    exit 1
-fi
-
-if [ $2 ]
-then
-    if [ $2 == "--delete-build" ]
+	if [ $1 == "clean" ]
     then
-        rm -r ./build/*
-    else
-        echo "Invalid argument for deletion"
+        rm -r ./src/build/*
+        rm -r ./test/build/*
     fi
 fi
