5592da5ec90cc49cf1ada0a22b6610e1f6c9f00d
Daniel Santos <dsantosp12@gmail.com>
2017-07-30 21:48:08 -0400

    Added API and in code docs of double linked list.

--- src/Linked_List/doubly_linked_list.h
+++ src/Linked_List/doubly_linked_list.h
@@ -24,13 +24,118 @@
 #ifndef DSTRUCTS_DOUBLY_LINKED_LIST_H
 #define DSTRUCTS_DOUBLY_LINKED_LIST_H
 
+#include <cstdio>
 #include "doubly_node.h"
 
 namespace DStructs {
+
+template <class T>
 class DLinkedList {
+ public:
+  /**
+   * \breif     Default constructor.
+   * */
+  DLinkedList();
+  /**
+   * \breif     Destructor
+   * */
+  ~DLinkedList();
+  /**
+   * \brief     Returns the data on front of the list. If the list is empty a
+   * std::out_of_range exception will be thrown.
+   *
+   * \returns   T&
+   *
+   * \note      O(1) time
+   * */
+  T& front() const;
+  /**
+   * \brief     Returns the data at the end of the list. If the list is empty
+   * a std::out_of_range exception will be thrown.
+   *
+   * \returns   T&
+   *
+   * \note      O(1) time.
+   * */
+  T& back() const;
+  /**
+   * \brief     Inserts the passed data, T&, to the list given the index iff
+   * index < size_ - 1
+   *
+   * \param     std::size_t index, const T& data
+   *
+   * \note      Careful, this is worst-case O(n) time.
+   * */
+  void put_at(std::size_t index, const T& data);
+  /**
+   * \brief     Returns the data at the given index. If the index is out of
+   * range, a std::out_of_range exception will be thrown.
+   *
+   * \param     std::size_t index
+   *
+   * \returns   T&
+   *
+   * \note      Careful, this is worst-time O(n) time.
+   * */
+  T& at(std::size_t index) const;
+  /**
+   * \brief     Insert a T& data on front of the list.
+   *
+   * \param     const T& data
+   *
+   * \note      O(1) time
+   * */
+  void put_front(const T& data);
+  /**
+   * \brief     Append T& data to the list.
+   *
+   * \param     const T& data
+   *
+   * \note      O(1) time
+   * */
+  void push_back(const T& data);
+  /**
+   * \brief     Remove the node on front of the list. If the list is empty a
+   * std::out_of_range exception will be thrown.
+   *
+   * \note      O(1) time.
+   * */
+  void pop_front();
+  /**
+   * \brief     Remove the node in the back of the list. If the list is empty
+   * a std::out_of_range exception will be thrown.
+   *
+   * \note      O(1) time.
+   * */
+  void pop_back();
+  /**
+   * \breif     Returns the size of the list.
+   * */
+  std::size_t size() const noexcept;
+  /**
+   * \brief     Return true is the list is empty, otherwise false.
+   * */
+  bool empty() const noexcept;
+  /**
+   * \brief     Remove all the nodes in the list.
+   *
+   * \note      Careful, this is best-case, average, and worst-case O(n) time.
+   * */
+  void erase();
+  /**
+   * \brief     Remove const T& data if it exist in the list. If successful,
+   * true will be returned, otherwise false.
+   *
+   * \param     const T& data to remove.
+   *
+   * \note      Careful, this is worst-case O(n) time.
+   * */
+  bool remove(const T& data);
 
  private:
-
+  DNode<T>* front_;       //< front node of the linked list
+  DNode<T>* tail_;        //< tail node of the linked list
+  unsigned int size_;     //< size of the linked list
 };
 
 }
