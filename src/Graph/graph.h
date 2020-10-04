7943a1e9d420cac4855c2ebc6357211c60e50d55
Daniel Santos <dsantosp12@gmail.com>
2020-10-04 15:32:12 -0400

    Graph data structure implementation

--- src/Graph/graph.h
+++ src/Graph/graph.h
@@ -2,36 +2,194 @@
 #define DSTRUCTS_GRAPH_H
 
 #include <memory>
+#include <unordered_set>
 
-#include "../Dynamic_Array/dynamic_array.h"
+namespace DStructs::graph {
+template<class T>
+struct node;
 
-namespace DStructs {
+template<class T>
+using node_ptr = std::shared_ptr<node<T>>;
 
-template <class T>
-struct GNode;
+template<class T>
+class graph;
 
-template <class T>
-using GNodePtr = std::shared_ptr<GNode<T>>;
+template<class T>
+class graph
+{
+public:
+    using value_type = T;
+    using element_type = node<value_type>;
+    using element_ptr_type = node_ptr<value_type>;
+    using container_type = std::unordered_set<element_ptr_type>;
+    using predicate_type =
+        std::function<bool(element_ptr_type, element_ptr_type)>;
 
-template <class T>
-struct GNode {
-    T _item;
+    graph() = default;
+    virtual ~graph() = default;
 
-    DynamicArray<GNodePtr<T>> _edges;
-};
+    // Accesors
+    const container_type& get_vertices() const { return _vertices; }
 
-template <class T>
-class Graph {
-    Graph() {}
+    // Modifiers
+    void add_vertex(element_ptr_type v)
+    {
+        if (!has_pointer(v)) {
+            _vertices.push_back(v);
+        }
+    }
 
-    ~Graph() {}
+    void add_edge(element_ptr_type dest, element_ptr_type src)
+    {
+        assert(dest);
+        assert(src);
 
-    
+        dest->add_edge(src);
+    }
+
+    // Creational methods
+    element_ptr_type make_vertex()
+    {
+        auto vertex = std::make_shared<element_type>();
+        _vertices.push_back(vertex);
+        return vertex;
+    }
+
+    element_ptr_type make_vertex(const value_type& value)
+    {
+        auto vertex = std::make_shared<element_type>(value);
+        _vertices.push_back(vertex);
+        return vertex;
+    }
+
+    element_ptr_type make_vertex(value_type&& value)
+    {
+        auto vertex = std::make_shared<element_type>(std::move(value));
+        _vertices.insert(vertex);
+        return vertex;
+    }
+
+    // Traversal strategy
+    template<typename Comp>
+    struct bfs
+    {
+        bool operator()(predicate_type pred,
+                        element_ptr_type src,
+                        element_ptr_type dest)
+        {
+            assert(src);
+            assert(dest);
+
+            Comp comp;
+            std::set<element_ptr_type> visited;
+            std::deque<element_ptr_type> q;
+            q.push_back(src);
+
+            while (!q.empty()) {
+                auto curr = q.front();
+                q.pop_front();
+
+                if (comp(dest, curr)) {
+                    return true;
+                } else {
+                    for (const auto& edge : curr->get_edges()) {
+                        if (visited.count(edge) == 0 && pred(curr, edge)) {
+                            visited.insert(curr);
+                            q.push_back(edge);
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+    };
+
+    // TODO: Implement depth-first search
+    struct dfs
+    {};
+
+    // Traversal
+    template<typename Strategy = bfs<std::equal_to<element_ptr_type>>>
+    bool traverse(element_ptr_type src,
+                  element_ptr_type dest,
+                  predicate_type pred)
+    {
+        if (!src && _vertices.empty()) {
+            return false;
+        }
+
+        return Strategy()(pred, src, dest);
+    }
+
+    // Visualization methods
+    void print_graph() const
+    {
+        for (typename decltype(_vertices)::size_type idx = 0;
+             idx < _vertices.size();
+             idx++) {
+            _vertices[idx]->print_node();
+        }
+    }
 
 private:
-    GNodePtr<T> _root;
+    bool has_pointer(element_ptr_type ptr) { return _vertices.count(ptr) != 0; }
+
+    container_type _vertices;
 };
 
-}
+template<class T>
+struct node
+{
+    using value_type = T;
+    using container_type = std::vector<node_ptr<value_type>>;
+
+    node() = default;
+
+    explicit node(const value_type& t)
+        : _item(t)
+    {}
+
+    explicit node(value_type&& t)
+        : _item(t)
+    {}
+
+    void print_node() const
+    {
+        std::cout << "[ " << _item << " ] -> [ ";
+
+        for (typename decltype(_edges)::size_type idx = 0; idx < _edges.size();
+             idx++) {
+            std::cout << _edges[idx]->_item;
+
+            if (_edges.size() - 1 != idx)
+                std::cout << ", ";
+        }
+
+        std::cout << " ]" << std::endl;
+    }
+
+    void set_value(const value_type& value) { _item = value; }
+    void set_value(value_type&& value) { _item = value; }
+
+    value_type get_value() const { return _item; }
+    const value_type& get_const_value() const { return _item; }
+
+    const container_type& get_edges() const { return _edges; }
+
+private:
+    friend graph<value_type>;
+
+    void add_edge(node_ptr<value_type> edge)
+    {
+        assert(edge);
+        _edges.push_back(edge);
+    }
+
+    value_type _item;
+    std::vector<node_ptr<value_type>> _edges;
+};
+
+} // namespace DStructs::graph
 
 #endif // DSTRUCTS_GRAPH_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,149 @@
 #include <iostream>
-#include <thread>
-#include <chrono>
+#include <list>
+#include <map>
+#include <memory>
+#include <queue>
+#include <set>
+#include <sstream>
 
-#include "DStructs.h"
+#include "Graph/graph.h"
 
-int main() {
-  auto list = new DStructs::LinkedList<int>();
+using namespace DStructs::graph;
 
-  std::thread t1([](auto l) {
-    for (int i = 0; i < 5; i++) {
-      l->push_back(i);
+struct step
+{
+    step(std::pair<int, int> pos, bool blocked = false)
+        : _pos(pos)
+        , _blocked(blocked)
+    {}
+
+    std::pair<int, int> _pos;
+    bool _blocked;
+};
+
+std::ostream&
+operator<<(std::ostream& os, const step& s)
+{
+    os << "(" << s._pos.first << ", " << s._pos.second << ")";
+    return os;
+}
+
+void
+print_path(
+    std::map<graph<step>::element_ptr_type, graph<step>::element_ptr_type> pred,
+    const graph<step>::element_ptr_type dest);
+
+std::string
+compute_move(const graph<step>::element_ptr_type src,
+             const graph<step>::element_ptr_type dest);
+
+int
+main()
+{
+    auto g = std::make_shared<graph<step>>();
+    auto _0_0 = g->make_vertex(std::move(step({ 0, 0 })));
+    auto _0_1 = g->make_vertex(std::move(step({ 0, 1 })));
+    auto _0_2 = g->make_vertex(std::move(step({ 0, 2 })));
+
+    auto _1_0 = g->make_vertex(std::move(step({ 1, 0 })));
+    auto _1_1 = g->make_vertex(std::move(step({ 1, 1 }, true)));
+    auto _1_2 = g->make_vertex(std::move(step({ 1, 2 }, true)));
+
+    auto _2_0 = g->make_vertex(std::move(step({ 2, 0 })));
+    auto _2_1 = g->make_vertex(std::move(step({ 2, 1 })));
+    auto _2_2 = g->make_vertex(std::move(step({ 2, 2 })));
+
+    g->add_edge(_0_0, _0_1);
+    g->add_edge(_0_0, _1_0);
+
+    g->add_edge(_0_1, _0_0);
+    g->add_edge(_0_1, _0_2);
+    g->add_edge(_0_1, _1_1);
+
+    g->add_edge(_0_2, _0_1);
+    g->add_edge(_0_2, _1_2);
+
+    g->add_edge(_1_0, _0_0);
+    g->add_edge(_1_0, _1_1);
+    g->add_edge(_1_0, _2_0);
+
+    g->add_edge(_1_1, _0_1);
+    g->add_edge(_1_1, _1_0);
+    g->add_edge(_1_1, _1_2);
+    g->add_edge(_1_1, _2_1);
+
+    g->add_edge(_1_2, _0_2);
+    g->add_edge(_1_2, _1_1);
+    g->add_edge(_1_2, _2_2);
+
+    g->add_edge(_2_0, _1_0);
+    g->add_edge(_2_0, _2_1);
+
+    g->add_edge(_2_1, _2_0);
+    g->add_edge(_2_1, _2_2);
+    g->add_edge(_2_1, _1_1);
+
+    g->add_edge(_2_2, _1_2);
+    g->add_edge(_2_2, _2_1);
+
+    auto vertices = g->get_vertices();
+
+    std::map<graph<step>::element_ptr_type, graph<step>::element_ptr_type> pred;
+
+    auto status = g->traverse(_0_0, _2_2, [&pred](auto curr, auto edge) {
+        bool accept = false;
+        if (edge->get_value()._blocked == false) {
+            accept = true;
+            pred.insert({ edge, curr });
+        }
+        return accept;
+    });
+
+    if (status) {
+        std::cout << "found it!" << std::endl;
+    } else {
+        std::cout << "no found!" << std::endl;
     }
-  }, list);
 
-  std::thread t2([](auto l) {
-    for (int i = 5; i < 10; i++) {
-      l->push_back(i);
+    print_path(pred, _2_2);
+
+    return 0;
+}
+
+std::string
+compute_move(const graph<step>::element_ptr_type src,
+             const graph<step>::element_ptr_type dest)
+{
+    auto src_val = src->get_value();
+    auto dst_val = dest->get_value();
+
+    if ((src_val._pos.first - dst_val._pos.first) > 0) {
+        return "N";
+    } else if ((src_val._pos.first - dst_val._pos.first) < 0) {
+        return "S";
+    } else if ((src_val._pos.second - dst_val._pos.second) > 0) {
+        return "W";
+    } else if ((src_val._pos.second - dst_val._pos.second) < 0) {
+        return "E";
+    } else {
+        assert(false);
+        return "";
     }
-  }, list);
+}
 
-  std::thread r([](auto l) {
-    for (int i = 0; i < 15; i++) {
-      l->forEach([](auto it) { std::cout << it << " "; });
-      std::cout << std::endl;
+void
+print_path(
+    std::map<graph<step>::element_ptr_type, graph<step>::element_ptr_type> pred,
+    const graph<step>::element_ptr_type dest)
+{
+    std::list<std::string> path;
+    auto curr = dest;
+
+    while (curr && pred[curr]) {
+        path.push_front(compute_move(pred[curr], curr));
+        curr = pred[curr];
     }
-  }, list);
 
-  t1.join();
-  t2.join();
-  r.join();
-
-  std::cout << list->size() << std::endl;
-
-  list->forEach([](auto it) { std::cout << it << " "; });
-
-  return 0;
+    std::for_each(
+        path.begin(), path.end(), [](auto p) { std::cout << p << std::endl; });
 }
